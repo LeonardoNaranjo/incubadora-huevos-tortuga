bfa444d83f847f73bd308d5ff181936df27f8f9e
LeonardoNaranjo <47719586+LeonardoNaranjo@users.noreply.github.com>
2022-05-27 17:14:29 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,15 @@ let Temperatura = 0
 basic.forever(function () {
     Temperatura = input.temperature()
     if (Temperatura < 26) {
+        soundExpression.slide.play()
         basic.showIcon(IconNames.Sad)
         basic.showString("T. Baja")
     } else if (Temperatura > 34) {
+        soundExpression.soaring.play()
         basic.showIcon(IconNames.Sad)
         basic.showString("T. Alta")
     } else {
+        soundExpression.happy.play()
         basic.showIcon(IconNames.Heart)
         basic.showString("T. Normal")
     }
